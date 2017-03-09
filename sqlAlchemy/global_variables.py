45e69121ea6bb01badba0bba39881e81ad3bdd2d
Akhil Ravichandran <akhilravichandran11@gmail.com>
2017-03-08 17:13:49 -0700

    mc

--- sqlAlchemy/global_variables.py
+++ sqlAlchemy/global_variables.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 sqlLite_database_name = 'smlProject.db'
 file_path = '/Users/ravichandran/Documents/Misc/Docs/ASU_Subjects/Spring_17/SML/Project/Code/Yelp-Trend-propagation---NetGel/data/'
-added_files = ['yelp_academic_dataset_checkin.csv']
+#added_files = ['yelp_academic_dataset_tip.csv']
+added_files = []
 file_names = ['yelp_academic_dataset_checkin.csv','yelp_academic_dataset_tip.csv']
